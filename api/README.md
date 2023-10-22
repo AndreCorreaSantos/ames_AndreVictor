8e62fe1b40aad7ef4cf64b5e13ccbd2362a3189c
rafaelcl292 <rafaelcl@al.insper.edu.br>
2023-10-22 16:38:44 -0300

    api documentation

---
+++ api/README.md
@@ -0,0 +1,23 @@
+# API
+
+Firstly install all packedges needed for the API
+
+``` bash
+pip install -r requirements.txt
+```
+
+Then run the API
+
+``` bash
+uvicorn main:app
+```
+
+The API will be running on
+http://127.0.0.1:8000/predict
+
+For a easy way to test the API, you can go to
+http://127.0.0.1:8000/docs
+
+The documentation is available on
+http://127.0.0.1:8000/redoc
+
--- api/main.py
+++ api/main.py
@@ -2,25 +2,53 @@ from fastapi import FastAPI, Body
 from pydantic import BaseModel
 from typing import List
 import joblib
+import csv
 
 
 app = FastAPI()
 
 
+with open('xtest.csv', 'r') as f:
+    x = list(csv.reader(f))[0]
+
+
 class Data(BaseModel):
     data: List[float]
 
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "data": [float(i) for i in x]
+            }
+        }
+    }
 
-# loading the model (model.pkl)
+
+class Prediction(BaseModel):
+    prediction: float
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "prediction": 4.778
+            }
+        }
+    }
+
+
 path = "../models/best_model.pkl"
 model = joblib.load(path)
 
 
 @app.post("/predict")
-def predict(data: Data = Body(...)):
-    print(data)
+def predict(data: Data = Body(...)) -> Prediction:
+    '''
+    This function takes in a list of 164 features and returns a prediction
+
+    We assume that the data is already preprocessed
+    '''
     if len(data.data) != 164:
-        return {"error": "invalid data"}
+        return {"error": "data must be a list of 164 features"}
     prediction = model.predict([data.data])
     return {"prediction": prediction[0]}
 
